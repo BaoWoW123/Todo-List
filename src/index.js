b009867573116fec4b5f0d2add710d6eebb6dbba
BaoWoW <baonguyen9392@gmail.com>
2022-11-23 16:35:24 -0800

    Create HTML/CSS Layout & JS Files
    Made and styled the html page with CSS and images.
    Added empty js files with needed functions/modules.
    Used webpack to load html page.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './style.css'
+import UI from './modules/UI'
+import addTask from './modules/addTask'
+import removeTask from './modules/removeTask'
+import storage from './modules/storage'
+import projects from './modules/projects'
+
+
